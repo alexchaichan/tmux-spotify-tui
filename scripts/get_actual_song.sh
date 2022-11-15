7bc9dabe216a09024bc874c0e84dc730c711c856
alexchaichan <a.chaichan@icloud.com>
2022-11-15 12:00:52 +0100

    fixed issue (liked songs)

--- scripts/get_actual_song.sh
+++ scripts/get_actual_song.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 string="$(spt playback)"
 firstletter=${string:0:1}
+thirdletter=${string:2:2}
 
-if [[ "$firstletter" == "▶" ]]; then
+if [[ "$firstletter" == "▶" || "$firstletter" == "♥" && "$thirdletter" != "⏸ "  ]]; then
 spt playback
 fi
